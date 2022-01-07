9e9df209071d932010a9aabb7e2ab964892b5636
Nathan Conn <nconn34@gmail.com>
2022-01-07 10:50:04 -0800

    Wrote first test

---
+++ __tests__/test.js
@@ -0,0 +1,9 @@
+import { TestScheduler } from 'jest'
+import SpaceAge from '../src/spaceage.js'
+
+describe('SpaceAge', ()=>{
+  test('should correctly return a users converted age on the specified planet', () =>{
+      const age = new SpaceAge(40);
+      expect(age.mercuryAge(40)).toEqual(9.6);
+  });
+})
