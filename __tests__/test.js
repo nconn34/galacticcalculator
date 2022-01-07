4d24f119cfa4f93769c92e79407ce7528e9764a5
Nathan Conn <nconn34@gmail.com>
2022-01-07 10:55:45 -0800

    Wrote second test

--- __tests__/test.js
+++ __tests__/test.js
@@ -5,5 +5,6 @@ describe('SpaceAge', ()=>{
   test('should correctly return a users converted age on the specified planet', () =>{
       const age = new SpaceAge(40);
       expect(age.mercuryAge(40)).toEqual(9.6);
+      expect(age.venusAge(40)).toEqual(24.8)
   });
 })
--- src/spaceage.js
+++ src/spaceage.js
@@ -1,6 +1,10 @@
 export default class SpaceAge{
-    mercuryAge(number){
+  mercuryAge(number){
     this.age = number*.24
+    return this.age
+}
+  venusAge(number){
+    this.age = number*.62
     return this.age
 }
 }
