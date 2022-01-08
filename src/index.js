8e4bcf64b219c59b33b8cd117fc7ab7a439b11e8
Nathan Conn <nconn34@gmail.com>
2022-01-07 16:30:10 -0800

    Added import statements to begin building UI

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import SpaceAge from './spaceage.js';
+import LifeExp from './lifeexpect.js'; 
+import './css/styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
