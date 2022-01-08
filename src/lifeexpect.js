d4a419798b5455114ca97b1d71a73024733820c0
Nathan Conn <nconn34@gmail.com>
2022-01-07 19:20:33 -0800

    Fixed function for life expectency on each planet

--- src/lifeexpect.js
+++ src/lifeexpect.js
@@ -1,21 +1,22 @@
 export default class LifeExp{
-    mercuryLife(number){
-        this.life = Math.abs(Math.round(79-number*.24))
-        this.left = Math.round(this.life*.24)
-        return this.left
-    }
-      venusLife(number){
-        this.life = Math.abs(Math.round(79-number*.62))
-        this.left = Math.round(this.life*.62)
-        return this.left
-    }
-      marsLife(number){
-        this.life = Math.abs(Math.round(79-number*1.88))
-        this.left = Math.round(this.life*1.88)
-        return this.left
-    }
-      jupiterLife(number){
-        this.life = Math.abs(Math.round(79-number*11.86))
-        return this.life
-    }
+  mercuryLife(number){
+    this.life = Math.round(number*.24);
+    this.left = Math.round(79*.24)-this.life;
+    return this.left;
+  }
+  venusLife(number){
+    this.life = Math.round(number*.62);
+    this.left = Math.round(79*.62-this.life);
+    return this.left;
+  }
+  marsLife(number){
+    this.life = Math.round(number*1.88);
+    this.left = Math.round(79*1.88-this.life);
+    return this.left;
+  }
+  jupiterLife(number){
+    this.life = Math.round(number*11.86);
+    this.left = Math.round(79*11.86-this.life);
+    return this.left;
+  }
 }
