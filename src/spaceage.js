96c336e5db1ae71d6729584bb4c55b4cafceb9d0
Nathan Conn <nconn34@gmail.com>
2022-01-07 19:21:02 -0800

    Fixed functions for converted galactic age

--- src/spaceage.js
+++ src/spaceage.js
@@ -1,18 +1,18 @@
 export default class SpaceAge{
   mercuryAge(number){
-    this.age = Math.round(number*.24)
-    return this.age
-}
+    this.age = Math.round(number*.24);
+    return this.age;
+  }
   venusAge(number){
-    this.age = Math.round(number*.62)
-    return this.age
-}
+    this.age = Math.round(number*.62);
+    return this.age;
+  }
   marsAge(number){
-    this.age = Math.round(number*1.88)
-    return this.age
-}
+    this.age = Math.round(number*1.88);
+    return this.age;
+  }
   jupiterAge(number){
-    this.age = Math.round(number*11.86)
-    return this.age
+    this.age = Math.round(number*11.86);
+    return this.age;
+  }
 }
-};
