81be3c562cede0c873d036343cffac4fd0f860ff
Nathan Conn <nconn34@gmail.com>
2022-01-07 19:41:02 -0800

    Altered life expectency functions to better fit expected output

--- src/lifeexpect.js
+++ src/lifeexpect.js
@@ -1,22 +1,22 @@
 export default class LifeExp{
   mercuryLife(number){
     this.life = Math.round(number*.24);
-    this.left = Math.round(79*.24)-this.life;
+    this.left = Math.round(79-this.life);
     return this.left;
   }
   venusLife(number){
     this.life = Math.round(number*.62);
-    this.left = Math.round(79*.62-this.life);
+    this.left = Math.round(79-this.life);
     return this.left;
   }
   marsLife(number){
     this.life = Math.round(number*1.88);
-    this.left = Math.round(79*1.88-this.life);
+    this.left = Math.round(79-this.life);
     return this.left;
   }
   jupiterLife(number){
     this.life = Math.round(number*11.86);
-    this.left = Math.round(79*11.86-this.life);
+    this.left = Math.abs(79-this.life);
     return this.left;
   }
 }
